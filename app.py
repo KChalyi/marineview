fd5bdcd09ba0e697a59f58188a08fb1a8651979f
KChalyi <125888138+KChalyi@users.noreply.github.com>
2023-02-20 16:53:48 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import openpyxl
+
+df2=pd.read_excel("./test.xlsx")
+st.dataframe(df2)
+st.title("A Simple Streamlit Web App")
+name=st.text_input("Enter your name",' ')
+st.write(f"Hello {name}!")
+x=st.slider("Select an integer x",0,10,1)
+y=st.slider("Select an integer y",0,10,1)
+df=pd.DataFrame({"x":[x],"y":[y],"x+y":[x+y]},index=["addition row"])
+st.write(df)
