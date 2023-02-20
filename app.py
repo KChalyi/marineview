3c9742a3c1de5014a047fb96d57f84ab4f4a8453
KChalyi <125888138+KChalyi@users.noreply.github.com>
2023-02-20 17:04:12 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import streamlit as st
 import pandas as pd
 import openpyxl
-
+st.title("Таблица куколдов")
+name=st.text_input("Имя смотрящего",' ')
 df2=pd.read_excel("./test.xlsx")
 st.dataframe(df2)
-st.title("A Simple Streamlit Web App")
-name=st.text_input("Enter your name",' ')
-st.write(f"Hello {name}!")
+st.write(f"Привет куколд {name}!")
 x=st.slider("Select an integer x",0,10,1)
 y=st.slider("Select an integer y",0,10,1)
 df=pd.DataFrame({"x":[x],"y":[y],"x+y":[x+y]},index=["addition row"])
